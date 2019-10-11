efaf8fd7b5d347d7c8d0085c1f3c2173fb6b663d
Rebalias <Rebalias@gmail.com>
2019-10-11 13:54:55 -0400

    map_mask now transforms map and poseupdate properly

--- src/map_mask.py
+++ src/map_mask.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import rospy
 from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseWithCovarianceStamped as PoseCv
 
-def callback(map1):
+offs = 0
+
+def mapMask(map1):
   map2 = OccupancyGrid()
   map2.header = map1.header
   map2.info = map1.info
+  global offs
+  offs = map1.info.width
+  map2.info.origin.position.x = 0
+  map2.info.origin.position.y = 0
   map2.data = [0 if x==-1 else x for x in map1.data]
-  pub.publish(map2)
+  pubM.publish(map2)
+
+def poseMask(pose1):
+  pose2 = PoseCv()
+  pose2 = pose1
+  pose2.pose.pose.position.x += offs/2
+  pose2.pose.pose.position.y += offs/2
+  pubP.publish(pose2)
 
 rospy.init_node('map_mask')
-sub = rospy.Subscriber('map',OccupancyGrid,callback)
-pub = rospy.Publisher('map2',OccupancyGrid,latch=True)
+subM = rospy.Subscriber('map',OccupancyGrid,mapMask)
+subP = rospy.Subscriber('poseupdate',PoseCv,poseMask)
+pubM = rospy.Publisher('map2',OccupancyGrid,latch=True,queue_size=2)
+pubP = rospy.Publisher('pose2',PoseCv,latch=True,queue_size=5)
 rospy.spin()
